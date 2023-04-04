77d0f2e3ce3232b1f57d7eb1e6d3aa8aed5aef6d
chronoabi <abishekpoudel2014@gmail.com>
2023-04-04 14:37:47 +0545

    footer script import done

--- functions.php
+++ functions.php
@@ -13,17 +13,17 @@ function testtheme_register_styles(){
 //write wp_enqueue_script to call a specific function. before this you wrote style and thats why it wasnot working.
 add_action('wp_enqueue_scripts', "testtheme_register_styles");
  
+
 function testtheme_register_scripts(){
 
-    wp_enqueue_scripts('testtheme-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js',array(),'3.4.1',true);
+    wp_enqueue_script('testtheme-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js',array(),'3.4.1',true);
 
-    wp_enqueue_scripts('testtheme-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',array(),'3.4.1',true);
+    wp_enqueue_script('testtheme-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',array(),'1.16.0',true);
 
-    wp_enqueue_scripts('testtheme-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js',array(),'3.4.1',true);
+    wp_enqueue_script('testtheme-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js',array(),'4.4.1',true);
 
-    wp_enqueue_scripts('testtheme-scripts','https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js',array(),'3.4.1',true);
-
-
+    wp_enqueue_script('testtheme-main',get_template_directory_uri()."/assets/js/main.js",array(),'3.4.1',true);
 }
 
+add_action('wp_enqueue_scripts', "testtheme_register_scripts");
 ?>
